17fa0455d8566e655cea4a870061c13ff2c0219f
panda-sas <panda.sas20@gmail.com>
2021-12-07 12:31:23 -0600

    Finding and Selecting Particular Elements with BeautifulSoup

---
+++ Making Soup/main.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+# import lxml
+
+
+with open("website.html", encoding="utf8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+# print(soup.li)
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+
+
+# for tag in all_anchor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.get("class"))
+
+class_is_heading = soup.find_all(class_="heading")
+print(class_is_heading)
+
+h3_heading = soup.find_all("h3", class_="heading")
+print(h3_heading)
+
+name = soup.select_one(selector="#name")
+print(name)
